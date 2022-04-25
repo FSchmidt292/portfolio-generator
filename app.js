f6479b4421494fcfc56bd40f2ff5344f40609c18
Trey S <fjschmidt292@gmail.com>
2022-04-25 03:36:10 -0400

    utils files, linking between pages, finishing off the project

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const inquirer = require('inquirer');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
-const fs = require('fs');
+const inquirer = require('inquirer');
 
 const generatePage = require('./src/page-template.js');
 
@@ -144,11 +144,18 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-       if (err) throw new Error(err);
-
-       console.log('Page created! Check out index.html in this directory to see it!');
-     });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
